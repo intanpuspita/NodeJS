cb3429853147dd2036f00817a67b0fa9ac4340e1
Intan Puspitasari <intanpussss@gmail.com>
2018-06-22 09:15:15 +0700

    Create function to post message from given form

--- Exercise/AppChat/server.js
+++ Exercise/AppChat/server.js
@@ -5,22 +5,28 @@
  **/
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // Load static HTML File
 app.use(express.static(__dirname));
+// Use parser for body that contain json and URL
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-var messages = [
-    {name: 'Benedict', message:'Hi'},
-    {name: 'Cumberbatch', message:'Hello'}
-]
+var messages = [];
 
 app.get('/messages', (req, res) => {
     res.send(messages);
 });
 
-// Listen to port 3123
-var server = app.listen(3123, () =>{
+app.post('/messages', (req, res)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
+});
+
+// Listen to port 3000
+var server = app.listen(3000, () =>{
     console.log("Server is listening on port", server.address().port);
 });
 
